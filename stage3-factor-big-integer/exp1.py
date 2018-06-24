e8fce85540e19c30be3147cb75dccd1f14a43c4b
a-boy <avvboy@gmail.com>
2018-06-24 23:11:32 +0000

    gcdFactor

--- stage3-factor-big-integer/exp1.py
+++ stage3-factor-big-integer/exp1.py
@@ -1,7 +1,8 @@
-from factorlab import factor_with_square
+from factorlab import factor_with_square,gcdFactor
 p=1277
 b=2*p
 n=2**p-1
 print(p,n)
-g1,g2=factor_with_square(n,s=p*(p-1))
-print("factored:",g1,g2)
+#g1,g2=factor_with_square(n,s=p*(p-1))
+g=gcdFactor(n,s=p*2,aug=2*p*(p-1)+1,start=1)
+print("factored:",g)
--- stage3-factor-big-integer/factorlab.py
+++ stage3-factor-big-integer/factorlab.py
@@ -13,4 +13,21 @@ def factor_with_square(n,s=4):
                 print("g1=",g1,"g2=",g2,n%g1)
                 break
         #s=t-2
-    return (g1,g2)
+    return g1
+
+def gcdFactor(n,s=1024,aug=2293,start=1):
+    block=(n-start)//s
+    x=start
+    g=1;i=0
+    while i<s:
+        j=0
+        print('x',i,j)
+        while j<aug:
+            g=gcd(n,x)
+            if(g>1):
+                print("factored!g=",g,n%g,'i,j=',i,j)
+                return g
+            x+=1;j+=1
+        x+=block;i+=1
+    print('g==1 for n when s=',s,aug,start)
+    return 1
