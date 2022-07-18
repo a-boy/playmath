149d9e6cbd5676febf2072e4d6169b1e7ee8bb97
a-boy <a_boy@live.com>
2022-07-18 18:32:02 +0800

    生成四平方和分解表

---
+++ stage25-Sums-of-Four-Squares/sum-4-squares.v2.js
@@ -0,0 +1,37 @@
+//generate table of 4 squares sum below MAX.
+//run this code to get a well formated csv file.
+//node .\sum-4-squares.v2.js > 4-squares-sum.csv
+
+const MAX=25, MAXS=MAX*MAX;
+let s=[],t=[],sum=0;
+//Array(100).fill(0)
+for (let i = 0; i <= MAX; i++) {
+    let square=i*i;
+    s.push(square);     
+}
+
+for (let i = 0; i <= MAXS; i++) {
+    t.push([i,'','','','']);
+}
+
+console.log(`n, 'one square', 'two squares', 'three squares', 'four squares'`);
+for (let a = 1; a <= MAX; a++) {
+    t[s[a]][1]=a;
+    for(let b = 1,sum=s[a]+s[b]; b <= a && sum<=MAXS; b++,sum=s[a]+s[b]) {     
+        t[sum][2]+=`${a}~${b}|`;
+        for(let c = 1,sum=s[a]+s[b]+s[c]; c <= b && sum<=MAXS; c++,sum=s[a]+s[b]+s[c]) {     
+            t[sum][3]+=`${a}~${b}~${c}|`;
+            for(let d = 1,sum=s[a]+s[b]+s[c]+s[d]; d <= c && sum<=MAXS; d++,sum=s[a]+s[b]+s[c]+s[d]) {     
+                t[sum][4]+=`${a}~${b}~${c}~${d}|`
+            }
+        }
+        
+    }
+
+}
+
+//require('util').inspect.defaultOptions.maxArrayLength = MAXS+1;
+t.forEach(element => {
+    console.log(`${element[0]}, '${element[1]}', '${element[2]}', '${element[3]}', '${element[4]}'`) 
+});
+
